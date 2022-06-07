a97404f33ae68d3cddf5ae1f36258bf30e8c020c
Thomaz Erick Assis dos Santos <thomaz_erick@hotmail.com>
2022-06-07 14:12:25 -0300

    HOTFIX ENDPOINT "PROCURAR"

--- api/src/repo/connection.js
+++ api/src/repo/connection.js
@@ -1,6 +1,6 @@
 import mysql from 'mysql2/promise'
 
-const {con} = await mysql.createConnection({
+const con = await mysql.createConnection({
     host:process.env.MYSQL_HOST,
     user:process.env.MYSQL_USER,
     password:process.env.MYSQL_PWD,
